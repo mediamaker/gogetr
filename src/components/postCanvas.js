b9a12068e154d44c58ed774540fb93281cd2d4ca
John Cozen <johncozen@gmail.com>
2019-02-15 03:16:52 -0800

    working api calls wwith a functiuonal component

--- src/components/postCanvas.js
+++ src/components/postCanvas.js
@@ -1,71 +1,26 @@
-import React, { Fragment, useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-const useDataApi = (initialUrl, initialData) => {
-  const [data, setData] = useState(initialData);
-  const [url, setUrl] = useState(initialUrl);
-  const [isLoading, setIsLoading] = useState(false);
-  const [isError, setIsError] = useState(false);
-
-  const fetchData = async () => {
-    setIsError(false);
-    setIsLoading(true);
-
-    try {
-      const result = await axios(url);
-
-      setData(result.data);
-    } catch (error) {
-      setIsError(true);
-    }
-
-    setIsLoading(false);
-  };
-
-  useEffect(() => {
-    fetchData();
-  }, [url]);
-
-  const doGet = (event, url) => {
-    setUrl(url);
-    event.preventDefault();
-  };
-
-  return { data, isLoading, isError, doGet };
-};
-
 function PostCanvas() {
-  const [query, setQuery] = useState('redux');
-  const { data, isLoading, isError, doGet } = useDataApi(
-    'http://hn.algolia.com/api/v1/search?query=redux',
-    { hits: [] },
+
+const [data, setQuote] = useState({ })
+
+const fetchQuote = async () => {
+  const result = await axios(
+    'http://quotes.rest/qod.json',
   );
-  axios.all([
-    axios.get('http://quotes.rest/qod.json'),
-    axios.get('https://picsum.photos/200/300/?random ')
-  ])
-  .then(axios.spread((quote, picture) => {
-    // do something with both responses
-    console.log([quote, picture])
-
-  }));
-
-
-  return (
-    <Fragment>
-
-
-      {isError && <div>Something went wrong ...</div>}
-
-      {isLoading ? (
-        <div>Loading ...</div>
-      ) : (
-        console.log('fragment')
-
-
-      )}
-    </Fragment>
-  );
+  setQuote(result.data.contents);
+  console.log(result.data.contents);
 }
 
+useEffect( () => {
+  fetchQuote();
+}, []);
+
+return (
+<div>{data.quotes.map(item =>
+<div>{item.quote}</div>
+  )}
+  </div>
+    )}
 export default PostCanvas;
