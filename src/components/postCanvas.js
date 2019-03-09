3992cffcdec5103e97253d6da930862578ba4ca7
John Cozen <johncozen@gmail.com>
2019-03-08 23:13:31 -0800

    latest proto

--- src/components/postCanvas.js
+++ src/components/postCanvas.js
@@ -2,38 +2,42 @@ import React, { useState, useEffect, Suspense } from "react";
 import axios from "axios";
 import { render, Text, useApp, Stage, Sprite } from "@inlet/react-pixi";
 import * as PIXI from "pixi.js";
+import { stringify } from "querystring";
+// import { FILE } from "dns";
 // import Unsplash from 'unsplash-js';
  
 const QUOTES_AUTH_TOKEN = "dCMlAV8KJo78QHWt5B5YugeF";
 // const imageURL = 'https://picsum.photos/800/800'
 const imageURL =  "https://api.unsplash.com/photos/random/?client_id=81a8941ee89933005e14ec94eff0c572a97bd369869025f90a2e3b7f59fb1619"
+const quoteURL =  'http://quotes.rest/quote/random?&category=inspirational'
+// const quoteURL =  'http://quotes.rest/quote/random.json'
 
 function PostCanvas() {
   const [data, setQuote] = useState({
     quote: "Getting some good juju going..."
-    
   });
-  const [returnedImage, setImage] = useState({});
+  const [returnedImage, setImage] = useState('loading');
   const [textStyle, setTextStyle] = useState({});
-  let loader = new PIXI.loaders.Loader();
-  loader.add('mySprite', imageURL);
-  loader.load((loader, resources) => {
-    console.log(loader);
-});
+  const [filters, setFilters] = useState([]);
+
   useEffect(() => {
-    fetchQuote();
+    useQuote();
   }, []);
   useEffect(() => {
-    fetchTextStyle();
+    useTextStyle();
   }, []);
   useEffect(() => {
     fetchImage();
-  }, [loader.resources.mySprite.url]);
+  }, []);
+  useEffect(() => {
+    fetchFilters();
+  }, []);
 
-  const fetchTextStyle = async () => {
+  const useTextStyle = async () => {
     const style = new PIXI.TextStyle({
       fill: "#fcfbfd",
       fontStyle: "italic",
+      fontSize: 52,
       fontVariant: "small-caps",
       fontWeight: "bold",
       letterSpacing: 2,
@@ -46,39 +50,46 @@ function PostCanvas() {
     setTextStyle(style);
   };
 
-  const fetchQuote = async () => {
+  const useQuote = async () => {
     let result = await axios({
-      url: "http://quotes.rest/quote/random.json",
+      url: quoteURL,
       headers: {
         "x-theysaidso-api-secret": QUOTES_AUTH_TOKEN
       }
     });
-    console.log('quote result:' + result.data.contents);
+    console.log(result);
     setQuote(result.data.contents.quote);
   };
-
+  
   const fetchImage = async () => {
     const imageResult = await axios({
       url:
         imageURL
     });
+
     console.log(imageResult.data.urls.regular)
-    loader.reset()
-    loader.add('mySprite', imageResult.data.urls.regular);
-    // loader.resources.mySprite.url = imageResult.data.urls.regular
+    // setImage(localImage)
+    setImage(imageResult.data.urls.regular)
+
   };
+
+  const fetchFilters = () => {
+    const filter = new PIXI.filters.ColorMatrixFilter()
+    filter.brightness(.3)
+    setFilters([filter])
+  }
 
   return (
     <>
       <Suspense fallback={<div>Loading...</div>}>
-        <Stage>
+        <Stage> 
           <Suspense fallback={<div>Loading...</div>}>
-            <Sprite image={loader.resources.mySprite.url} scale={1} />
+            <Sprite filters={filters} image={returnedImage} scale={1} />
             <Text text={data} style={textStyle} x={50} y={50} />
           </Suspense>
         </Stage>
       </Suspense>
-      <button onClick={e => fetchQuote()}>Change Quote</button>
+      <button onClick={e => useQuote()}>Change Quote</button>
       <button onClick={e => fetchImage()}>Change Image</button>
     </>
   );
